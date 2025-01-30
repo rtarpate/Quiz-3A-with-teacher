3cc0281e31db4672ecf0b261d1dab6665f7f5352
Ashton Pate <arpate@amarillocollege.com>
2025-01-30 13:10:06 -0600

    add commints

--- Quiz 3A with teacher/Program.cs
+++ Quiz 3A with teacher/Program.cs
@@ -10,14 +10,16 @@ namespace Quiz_3A_with_teacher
     {
         static void Main(string[] args)
         {
+            // decalre number varibale
             int number = 0;
-
+            //decalre sum varibale
             int sum = 0;
-
+            //prints promt asking user to input a number
             Console.WriteLine(" input a number ");
-
+            //collects and convert user input
             number = Convert.ToInt32(Console.ReadLine());
 
+            //users loop to find all odd numbers in user input
           //  for(int i =0;i<number; i++)
           //  {
           //      if(i % 2 == 1)
@@ -25,14 +27,15 @@ namespace Quiz_3A_with_teacher
           //          sum = sum + i;
           //      }
           //  }
-          //
+          // //prints final sum of odd numbers
           //  Console.WriteLine(sum);
 
+            //uses loop to find all odd numbers in user input
             for(int i =1; i < number; i=i+2)
             {
                 sum = sum + i;
             }
-
+            //prints final sum of odd numbers
             Console.WriteLine(sum);
 
 
